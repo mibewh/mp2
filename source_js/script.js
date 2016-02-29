88ba36a8c165c23cfc4bf1f0bda412d677bae2c2
Michael <mwhitman95@gmail.com>
2016-02-29 08:21:01 +0000

    This site is like, suuuuuuuuuper responsive now. Also there are more components but whatever

--- source_js/script.js
+++ source_js/script.js
@@ -1,1 +1,71 @@
-// Write any custom javascript functions here
+$(document).ready(function() {
+
+	//Carousel
+  	$(".carousel").slick({
+        arrows: true,
+        dots: true,
+        lazyLoad: 'progressive',
+        autoplay: true,
+        infinite: true
+      });
+
+	// //Navbar
+	//Position indicator
+	var setActive = function(elem) {
+		$('.navopt').removeClass("active");
+		elem.addClass('active');
+	}
+	var setNav = function() {
+		var navOffset = 110;
+		var introPos = $('#home').offset().top - navOffset;
+		var videoPos = $('#action').offset().top - navOffset;
+		var tricolumnPos = $('#process').offset().top - navOffset;
+		var carouselPos = $('#crew').offset().top - navOffset;
+		var navPos = $(window).scrollTop();
+		if(navPos < videoPos) {
+			setActive($('#homeNav'));
+		}
+		else if(navPos < tricolumnPos) {
+			setActive($('#actionNav'));
+		}
+		else if(navPos < carouselPos) {
+			setActive($('#processNav'));
+		}
+		else {
+			setActive($('#crewNav'));
+		}
+		//Check bottom
+		if(navPos + $(window).height() >= $(document).height()-5) {
+			setActive($('#crewNav'));
+		}
+	}
+
+	//Expand/shrink navbar
+	var setBarSize = function() {
+		var pos = $(window).scrollTop();
+		if(pos >= 100) { //Smaller bar
+			$('.top-bar').stop().animate({
+				padding: '0px'
+			}, {duration: 30, queue: false});
+		}
+		else { //Larger Bar
+			$('.top-bar').stop().animate({
+				padding: '20px'
+			}, {duration: 30, queue: false});
+		}
+	}
+
+	$(document).scroll(function() {
+		setNav();
+		setBarSize();
+		//Hide the extension menu on scroll
+		if($(window).width() < 1024) { //Medium and under
+			$("#main-menu").hide(); //Hide the extension menu
+		}
+	});
+	// set the active nav element  and bar size at the beginning
+	setNav();
+	setBarSize();
+
+
+});
